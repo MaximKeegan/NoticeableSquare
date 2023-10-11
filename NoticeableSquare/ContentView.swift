82c3ede6e21d203da54887052a8aefd3c9f8b103
Maxim Keegan <keegan@tochka.com>
2023-10-11 22:55:16 +0500

    Initial Commit

---
+++ NoticeableSquare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoticeableSquare
+//
+//  Created by Maxim Keegan on 11.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoticeableSquare/NoticeableSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoticeableSquareApp.swift
+//  NoticeableSquare
+//
+//  Created by Maxim Keegan on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NoticeableSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
