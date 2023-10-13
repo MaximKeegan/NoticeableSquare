161899f2f197af8e8963402a7f98b90d911d1a9b
Maxim Keegan <keegan@tochka.com>
2023-10-13 16:06:18 +0500

    First

--- NoticeableSquare/ContentView.swift
+++ NoticeableSquare/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rectPosition = CGPoint(x: 100, y: 400)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 0) {
+                Rectangle()
+                    .fill(.white)
+                Rectangle()
+                    .fill(.pink)
+                Rectangle()
+                    .fill(.yellow)
+                Rectangle()
+                    .fill(.black)
+                
+            }
+            let rect = RoundedRectangle(cornerRadius: 15)
+                .frame(width: 100, height: 100)
+                .position(rectPosition)
+                .gesture(DragGesture().onChanged({ value in
+                    self.rectPosition = value.location
+                }))
+            
+            rect.foregroundColor(.white)
+                .blendMode(.difference)
+                .overlay(rect.blendMode(.hue))
+                .overlay(rect.foregroundColor(.white).blendMode(.overlay))
+                .overlay(rect.foregroundColor(.black).blendMode(.overlay))
         }
-        .padding()
     }
 }
 
